e8a7f392662fcc85311cfa9622c53734229af319
isabelvillaruiz <i.villa@alumnos.urjc.es>
2016-10-19 10:41:28 +0200

    Le pasamos como parametro el puerto e imprime el valor del Client_address

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
-
+import sys
 
 class EchoHandler(socketserver.DatagramRequestHandler):
     """
@@ -14,11 +14,13 @@ class EchoHandler(socketserver.DatagramRequestHandler):
 
     def handle(self):
         self.wfile.write(b"Hemos recibido tu peticion")
+        print(self.client_address)
         for line in self.rfile:
             print("El cliente nos manda ", line.decode('utf-8'))
 
 if __name__ == "__main__":
-    serv = socketserver.UDPServer(('', 6001), EchoHandler)
+    serv = socketserver.UDPServer(('', int(sys.argv[1])), EchoHandler)
+    
     print("Lanzando servidor UDP de eco...")
     try:
         serv.serve_forever()
