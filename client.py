95fb641b920ea98d509e777c8ff9943c8d08f618
isabelvillaruiz <i.villa@alumnos.urjc.es>
2016-10-23 19:08:01 +0200

    Ejercicio 6 a falta de mejorar

--- client.py
+++ client.py
@@ -17,8 +17,15 @@ LINE = ' '.join(sys.argv[3:])
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
     print("Enviando:", LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-    data = my_socket.recv(1024)
-    print('Recibido -- ', data.decode('utf-8'))
+    if LINE[0:8] ==  "register":
+        SIP_dir = LINE[8:]
+        body_msg = "RESGISTER " + "sip:" + SIP_dir  #+ " SIP/2.0" 
+        #No se como imprimir el \r\n\r porq lo ingora
+        LINE = body_msg
+        #print(LINE)
+        #my_socket.send(bytes(body_msg)
+        my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+        data = my_socket.recv(1024)
+        print('Recibido -- ', data.decode('utf-8'))
 
 print("Socket terminado.")
