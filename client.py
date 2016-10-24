ec39935774992c3fcde37834b6287bde3a950310
isabelvillaruiz <i.villa@alumnos.urjc.es>
2016-10-24 23:27:31 +0200

    TERMINADO

--- client.py
+++ client.py
@@ -11,12 +11,8 @@ import sys
 
 SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
-LINE = ' '.join(sys.argv[3:])     
+LINE = ' '.join(sys.argv[3:])
 
-
-
-
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((SERVER, PORT))
     print("Enviando:", LINE)
@@ -24,12 +20,12 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     Register = Words_LINE[0]
     SIP_dir = Words_LINE[1]
     Expires = int(Words_LINE[2])
-    if Register ==  "register":
-       
-        body_msg1 = "REGISTER " + "sip: " + SIP_dir + " " + " SIP/2.0\r\n" +  "Expires: " + str(Expires) +"\r\n"
+    if Register == "register":
+        body_msg1 = "REGISTER " + "sip: " + SIP_dir + " " + " SIP/2.0\r\n"
+    + "Expires: " + str(Expires) + "\r\n"
         LINE = body_msg1
-        print(LINE) 
-        my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+        print(LINE)
+        my_socket.send(bytes(LINE, 'utf-8') + b'\r\n\r\n')
         data = my_socket.recv(1024)
         print('Recibido -- ', data.decode('utf-8'))
 print("Socket terminado.")
--- server.py
+++ server.py
@@ -7,15 +7,7 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 import socketserver
 import sys
 import json
-import time 
- 
-''' NOTAS '''  
-'''
-# self.wfile.write(b"SIP/2.0 200 OK") #SIRVE PARA ESCRIBIR UN MENSAJE EN EL CLIENTE 
-Hay que conseguir que en caso de que sea 0 y este guardada esa direccion que nos la borre 
-separators=('' , ': ')     
-'''
-
+import time
 
 
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
@@ -23,11 +15,10 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     Echo server class
     """
     dicc = {}
- 
 
-    def register2json (self):
-        json_file = open("registered.json","w")
-        json.dump(self.dicc,json_file, indent = 4)
+    def register2json(self):
+        json_file = open("registered.json", "w")
+        json.dump(self.dicc, json_file)
         json_file.close()
 
     def json2registered(self):
@@ -36,59 +27,38 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
             print("Estamos probando")
             with open("registered.json") as JsonFile:
                 self.dicc = json.load(JsonFile)
-        except: 
+        except:
                 pass
 
     def handle(self):
         self.wfile.write(b"Hemos recibido tu peticion")
         self.json2registered()
         print(self.client_address)
-        
-
         texto = self.rfile.read()
         print("El cliente nos manda ", texto.decode('utf-8'))
         LINE_S = texto.decode('utf-8')
         print(LINE_S)
-        Words_LINES_S = LINE_S.split() 
-             
+        Words_LINES_S = LINE_S.split()
         dir_sip = Words_LINES_S[2]
         Expires = int(Words_LINES_S[5])
-        
         actual_time = time.time()
         expire_time = actual_time + Expires
-        str_actual_time = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(actual_time))
-        str_expire_time = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(expire_time))
-        
-        
+        str_ac_t = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(actual_time))
+        str_ex_t = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(expire_time))
         if (Expires) != 0:
-            
-            self.dicc[dir_sip] = [self.client_address[0], str_expire_time]
-           
-            
-            
-            
-                #print(self.client_address)
-                #print(self.client_address[0])
-                #print(self.client_address[1])
-                #Prueba de que son lo que son [Logico]
-            print(self.dicc) #prueba de que guarda bien en el diccionario
+            self.dicc[dir_sip] = [self.client_address[0], str_ex_t]
+            print(self.dicc)
             print()
-            print("SIP/2.0 200 OK")
-
-        
-
-        elif (Expires) == 0 :
+            self.wfile.write(b"SIP/2.0 200 OK")
+        elif (Expires) == 0:
             if dir_sip in self.dicc:
                 del self.dicc[dir_sip]
-         
             self.register2json()
-            print(self.dicc) #prueba de que guarda bien en el diccionario
+            print(self.dicc)
             print()
-            print("SIP/2.0 200 OK") 
-            
-        
+            self.wfile.write(b"SIP/2.0 200 OK")
+        self.wfile.write(b" SIP/2.0 200 OK\r\n\r\n")
 
-    
 if __name__ == "__main__":
     serv = socketserver.UDPServer(('', int(sys.argv[1])), SIPRegisterHandler)
     print("Lanzando servidor UDP de eco...")
